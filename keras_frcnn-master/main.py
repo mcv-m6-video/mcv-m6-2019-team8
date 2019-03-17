b92c29dccafd7260fe6d73f12826d605bbb308ee
AnaCaballero89 <32468326+AnaCaballero89@users.noreply.github.com>
2019-03-17 14:46:00 -0400

    CNN running

--- keras_frcnn-master/main.py
+++ keras_frcnn-master/main.py
@@ -4,10 +4,11 @@ import cv2
 
 def main():
 
-    detector = FasterRCNNDetector(model_path='./model/kitti_fcnn_last.hdf5')
+    detector = FasterRCNNDetector(model_path='./weights/model_frcnn.hdf5')
 
     img = cv2.imread('images/000000.png')
     detector.detect_on_image(img)
+
 
 
 if __name__ == '__main__':
